a8224cab4a6ca54cf2c2e5a4f6401f540967c41c
mohit04gits <mohitgupta1834@gmail.com>
2025-03-21 12:39:37 +0530

    Update EditProfile.jsx

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
-import UserCard from "./userCard";
+
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
